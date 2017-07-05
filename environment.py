bfe7ce50e188279cc2e57bda9dd7efc92fa4a0f6
James de Lisle <lpko908@gmail.com>
2017-07-05 14:11:11 +0900

    tiedup

--- environment.py
+++ environment.py
@@ -24,16 +24,18 @@ class Environment:
     def DELTA_R_F( self ):
         
         VAL = self.value_set
+        DV = VAL[ 'constants' ][ 'd_vector' ]
 
         a = 1.764 / ( 2 * np.pi ) 
         b = np.tanh( np.sqrt( VAL[ 'constants' ][ 'T_c' ] / VAL[ 'temperature' ] - 1.0 ) ) 
         c = np.tanh( VAL[ 'R_now' ] ) * np.exp( 1j * VAL[ 'P_now' ] )
-        d = np.sin( VAL[ 'k_polar' ] ) * np.exp( 1j * VAL[ 'k_azimuthal' ] )
+        d = np.sin( VAL[ 'k_polar' ] ) * ( ( DV[ 'a1' ] - DV[ 'a4' ] ) * np.cos( VAL[ 'k_azimuthal' ] ) + 1j * ( DV[ 'a2' ] + DV[ 'a3' ] ) * np.sin( VAL[ 'k_azimuthal' ] ) ) 
+        e = np.exp( 1j * VAL[ 'R_now' ] * np.cos( VAL[ 'P_now' ] ) * VAL[ 'constants' ][ 'supercurrent_phase' ] )
         
-        if np.abs( a * b * c * d ) > 1.0:
+        if np.abs( a * b * c * d * e ) > 1.0:
             raise RuntimeWarning( 'The magnitude of the gap is greater than 1' )
         
-        return a * b * c * d
+        return a * b * c * d * e
 
     def DELTA_R_B( self ):
 
@@ -42,16 +44,18 @@ class Environment:
     def DELTA_A_F( self ):
         
         VAL = self.value_set
+        DV = VAL[ 'constants' ][ 'd_vector' ]
 
         a = 1.764 / ( 2 * np.pi ) 
         b = np.tanh( np.sqrt( VAL[ 'constants' ][ 'T_c' ] / VAL[ 'temperature' ] - 1.0 ) ) 
         c = np.tanh( VAL[ 'R_now' ] ) * np.exp( 1j * VAL[ 'P_now' ] )
-        d = np.sin( VAL[ 'k_polar' ] ) * np.exp( 1j * VAL[ 'k_azimuthal' ] )
+        d = np.sin( VAL[ 'k_polar' ] ) * ( ( DV[ 'a1' ] - DV[ 'a4' ] ) * np.cos( VAL[ 'k_azimuthal' ] ) + 1j * ( DV[ 'a2' ] + DV[ 'a3' ] ) * np.sin( VAL[ 'k_azimuthal' ] ) ) 
+        e = np.exp( 1j * VAL[ 'R_now' ] * np.cos( VAL[ 'P_now' ] ) * VAL[ 'constants' ][ 'supercurrent_phase' ] )
         
-        if np.abs( a * b * c * d ) > 1.0:
+        if np.abs( a * b * c * d * e ) > 1.0:
             raise RuntimeWarning( 'The magnitude of the gap is greater than 1' )
-
-        return a * b * c * d
+        
+        return a * b * c * d * e
 
     def DELTA_A_B( self ):
 
