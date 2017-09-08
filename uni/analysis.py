e8095a13de73eec9b22b5b20ba7687c288d48ad6
James de Lisle <lpko908@gmail.com>
2017-09-08 15:53:09 +0900

    added uniform ldos class

---
+++ uni/analysis.py
@@ -0,0 +1,20 @@
+from ana.unidos import LDOS
+import os
+
+
+data_folder = "data/"
+
+
+def getFiles(self):
+
+    return [f for f in os.listdir(data_folder)
+            if os.path.isfile(os.path.join(data_folder, f)) and '-0-' in f]
+
+files = sorted(getFiles())
+
+for f in files:
+
+    L = LDOS(f)
+    print(L.compute())
+
+
