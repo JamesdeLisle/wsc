56db969b0e7bdb8835343d0241abdc791ff6d17a
James de Lisle <lpko908@gmail.com>
2017-09-19 14:47:31 +0900

    ret

--- gen/env.py
+++ gen/env.py
@@ -44,17 +44,17 @@ class Environment(object):
     @property
     def sigmaR(self):
 
-        return -1j * 0.01
+        return -1j * 0.001
 
     @property
     def sigmaA(self):
 
-        return 1j * 0.01
+        return 1j * 0.001
 
     @property
     def sigmaK(self):
 
-        return 1j * 0.01
+        return 1j * 0.001
 
     @property
     def hamR(self):
