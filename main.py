3a9d67141c6894a2c3a1e79b188b403cc6278ba0
James de Lisle <lpko908@gmail.com>
2017-09-25 11:02:09 +0900

    ret

--- main.py
+++ main.py
@@ -30,8 +30,8 @@ if __name__ == '__main__':
     L.nKAzimu = 100
     L.nTemp = 1
     L.nAlpha = 1
-    L.energyMin = -0.1
-    L.energyMax = 0.1
+    L.energyMin = -1
+    L.energyMax = 1
     L.kPolarMin = 0.0
     L.kPolarMax = np.pi/2
     L.kAzimuMin = 0.0    
