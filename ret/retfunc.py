7dee2fec41f5f922bb8d28e7a6f2b69c4aff9520
James de Lisle <lpko908@gmail.com>
2017-09-27 14:26:21 +0900

    updated ret

--- ret/retfunc.py
+++ ret/retfunc.py
@@ -21,13 +21,9 @@ class Function:
         rv = np.zeros(shape=(2, 2), dtype=np.complex128)
         rv[0, 1] = -E.deltaR * (1 - V.dg0[0, 0] / (2 * np.conj(E.deltaR)))
         rv[1, 0] = np.conj(E.deltaR) * (1 + V.dg0[0, 0] / (2 * E.deltaR))
-        rv[0, 0] = (1.0 / delsq) * (epsil * (2 * delsq + 1j * V.lim.B_z
-                                             * V.dg0[1, 1])
-                                    + np.conj(E.deltaR) * 1j * V.lim.B_z
-                                    * V.dg0[0, 1])
-        rv[1, 1] = (1.0 / delsq) * (-epsil * (2 * delsq + 1j * V.lim.B_z
-                                              * V.dg0[1, 1])
-                                    - np.conj(E.deltaR) * 1j * V.lim.B_z
-                                    * V.dg0[1, 0])
+        rv[0, 0] = 1j * (2 * epsil * rv[0, 1] - V.lim.B_z * V.dg0[0, 1]) / \
+            (2 * E.deltaR)
+        rv[1, 1] = -1j * (2 * epsil * rv[1, 0] + V.lim.B_z * V.dg0[1, 0]) / \
+            (2 * np.abs(E.deltaR))
 
         return rv
