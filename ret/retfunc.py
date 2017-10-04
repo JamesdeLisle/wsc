a4d16b152099e8efed8927f93a72433110a8388d
James de Lisle <lpko908@gmail.com>
2017-10-04 14:11:06 +0900

    updated ret

--- ret/retfunc.py
+++ ret/retfunc.py
@@ -22,16 +22,17 @@ class Function:
         else:
             bsign = 1.0
 
-        f = E.deltaR * (1 - 1j * V.lim.B_z * bsign * V.dg0[0, 0] / (2 * delsq))
-        f_bar = -np.conj(E.deltaR) * (1 - 1j * V.lim.B_z * bsign * V.dg0[1, 1] /
+        f = -E.deltaR * (1 - 1j * V.lim.B_z * bsign * V.dg0[0, 0] / (2 * delsq))
+        f_bar = np.conj(E.deltaR) * (1 - 1j * V.lim.B_z * bsign * V.dg0[1, 1] /
                                       (2 * delsq))
 
         print V.dg0
         rv = np.zeros(shape=(2, 2), dtype=np.complex128)
         rv[0, 1] = f
         rv[1, 0] = f_bar
-        rv[0, 0] = (1.0 / (2 * E.deltaR)) \
-            * (-2 * epsil * f + 0 * 1j * V.lim.B_z * bsign * V.dg0[0, 1])
+        rv[0, 0] = (1.0 / (2 * E.deltaR)) * (-2 * epsil * f
+                                             + 1j * V.lim.B_z
+                                             * bsign * V.dg0[0, 1])
         rv[1, 1] = -rv[0, 0]
 
         return rv
