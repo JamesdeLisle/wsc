6445dfdc5d011081a9d331f2a9450a2ad15cd7fb
James de Lisle <lpko908@gmail.com>
2017-10-04 14:22:57 +0900

    updated ret

--- gen/main.py
+++ gen/main.py
@@ -61,7 +61,7 @@ class Main:
             self.DATA = {}
 
             ######################################
-            if self.order == '1':
+            if self.order == True:
                 runs = self.P.getRun(iT, iE, self.strings[0])
                 for run in runs:
                     worker(run)
