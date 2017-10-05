cbc70b07ecdb7a9ee9d108f4e3630970f0c3dd15
James de Lisle <lpko908@gmail.com>
2017-10-05 16:38:50 +0900

    updated ret

--- ret/retfunc.py
+++ ret/retfunc.py
@@ -16,29 +16,9 @@ class Function:
         V = self.runVal
         E = self.envi
 
-        delsq = E.deltaR * np.conj(E.deltaR)
-        epsil = V.ener + 1j * 1e-6 - E.sigmaR
-        if V.Theta < 0.0:
-            bsign = -1.0
-        else:
-            bsign = 1.0
+        tau3 = np.eye(2)
+        tau3[1, 1] = -1.0
+        lam = V.ener * tau3 - E.hamR
+        rv = np.linalg.inv(lam) * 1j * V.lim.B_z * V.dg0
 
-        if False:
-            f = E.deltaR * (1 - 1j * V.lim.B_z * bsign * V.dg0[0, 0] / (2 * delsq))
-            f_bar = -np.conj(E.deltaR) * (1 - 1j * V.lim.B_z * bsign * V.dg0[1, 1]
-                                      (2 * delsq))
-
-            rv = np.zeros(shape=(2, 2), dtype=np.complex128)
-            rv[0, 1] = f
-            rv[1, 0] = f_bar
-            rv[0, 0] = (1.0 / (2 * E.deltaR)) * (-2 * epsil * f
-                                                 + 1j * V.lim.B_z
-                                                 * bsign * V.dg0[0, 1])
-            rv[1, 1] = -rv[0, 0]
-
-        self.runVal.alpha = 1.0
-        A = unif.Function(self.runVal)
-        rv = A.gR
-        rv[0, 0] = rv[0, 0] +
-        
         return rv
