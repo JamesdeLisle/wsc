da2371c9e4b4efdce83bd23be12320ee40e9e184
James de Lisle <lpko908@gmail.com>
2017-10-25 17:40:49 +0900

    keld

--- ana/magnetisation.py
+++ ana/magnetisation.py
@@ -46,6 +46,12 @@ class MAG:
                             + self.P['3']['up'].data['gK'][iXi, iTheta] \
                             - self.P['1']['dn'].data['gK'][iXi, iTheta] \
                             - self.P['3']['dn'].data['gK'][iXi, iTheta]
+                    print self.P['1']['up'].data['gK'][iXi, iTheta]
+                    print self.P['1']['dn'].data['gK'][iXi, iTheta]
+                    print self.P['3']['up'].data['gK'][iXi, iTheta]
+                    print self.P['3']['dn'].data['gK'][iXi, iTheta]
+                    print g
+                    print '#####'
                     hTheta += np.trace(np.dot(fm.p3(), g))
                     hTheta /= 8 * np.pi * np.pi
                     hTheta *= self.lim.dKAzimu
